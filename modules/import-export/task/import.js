c273fbbdb9ce0115a7eed7c8e2d881d49d6c0177
Christian Murphy <Christian.Murphy@asu.edu>
2015-09-18 15:41:33 -0700

    import users

--- modules/import-export/task/import.js
+++ modules/import-export/task/import.js
@@ -1,22 +1,62 @@
-/* eslint no-sync: 0, max-nested-callbacks: [2, 2] */
+/* eslint no-sync: 0, max-nested-callbacks: [2, 2], no-else-return: 0 */
 'use strict';
 
 const libxml = require('libxmljs');
 const fs = require('fs');
 const path = require('path');
 
+const User = require('../../user/model/user');
+
 const schema = fs.readFileSync(path.join(__dirname, 'schema.xsd'));
-const parsedSchema = libxml.parseXml(schema);
+const parsedSchema = libxml.parseXmlString(schema);
 
 module.exports = function (location) {
+    let parsedDateset;
+
     return readFilePromise(location).then(function (dataset) {
-        const parsedDateset = libxml.parseXml(dataset);
+        // parse the new file
+        parsedDateset = libxml.parseXmlString(dataset);
+        // validate the file
         const isValid = parsedDateset.validate(parsedSchema);
 
         return {
             valid: isValid,
             errors: parsedDateset.validationErrors
         };
+    }).then(function (result) {
+        // if the result is valid generate users and teams
+        if (result.valid) {
+            // find all the users
+            const users = parsedDateset.find('//user');
+            const promises = [];
+
+            // for each user
+            for (let index = 0; index < users.length; index++) {
+                const currentUser = users[index];
+
+                // create a new database object
+                promises.push(
+                    User.create({
+                        name: currentUser.get('first-name').text() + ' ' + currentUser.get('last-name').text(),
+                        modules: {
+                            github: {
+                                username: currentUser.get('github').text()
+                            },
+                            taiga: {
+                                email: currentUser.get('email').text()
+                            }
+                        }
+                    })
+                );
+            }
+
+            // wait for all users to be created
+            return Promise.all(promises).then(function () {
+                return result;
+            });
+        } else {
+            return result;
+        }
     });
 };
 
