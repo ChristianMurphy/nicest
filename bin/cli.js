23457f326e55059f3ec01d0aa1af4b627b1790d3
Christian Murphy <Christian.Murphy@asu.edu>
2015-10-21 12:41:46 -0700

    replace yargs cli configuration with nicest.json

--- bin/cli.js
+++ bin/cli.js
@@ -1,52 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
-const hostname = require('os').hostname;
-const version = require('../package.json').version;
-
-// Get options from terminal
-const argv = require('yargs')
-    .usage('nicest --github-client <value> --github-secret <value> [options]')
-    .option('github-client', {
-        describe: 'github client application token',
-        demand: true
-    })
-    .option('github-secret', {
-        describe: 'github secret application token',
-        demand: true
-    })
-    .option('p', {
-        alias: 'port',
-        default: 3000,
-        describe: 'port to run server on'
-    })
-    .option('db', {
-        alias: 'database',
-        default: 'mongodb://localhost/nicest',
-        describe: 'mongo database connection'
-    })
-    .option('host', {
-        default: hostname,
-        describe: 'server hostname'
-    })
-    .option('prefix', {
-        default: '',
-        describe: 'prefix for the routes'
-    })
-    .option('token', {
-        default: 'password',
-        describe: 'token for hashing session data'
-    })
-    .help('help')
-    .alias('h', 'help')
-    .version(version)
-    .alias('v', 'version')
-    .argv;
+const configuration = require('../nicest.json');
 
 const database = require('../lib/database');
-const server = require('../lib/server').setup(argv);
+const server = require('../lib/server').setup(configuration);
 
-database(argv.database);
+database(configuration);
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
