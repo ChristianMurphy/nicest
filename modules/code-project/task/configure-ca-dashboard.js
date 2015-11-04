a3fa94e6c12b0e6761c9902c6d75f2603d4b184c
Christian Murphy <Christian.Murphy@asu.edu>
2015-11-04 10:18:36 -0700

    add JSDoc to the CA Dashboard configuration code

--- modules/code-project/task/configure-ca-dashboard.js
+++ modules/code-project/task/configure-ca-dashboard.js
@@ -1,27 +1,49 @@
 /**
  * @module ConfigureCaDashboard
  */
+
 'use strict';
 
 const mongoose = require('mongoose');
 
-const Members = new mongoose.Schema({
+/**
+ * Member is metadata for a single user
+ * @typedef {Object} Member
+ * @property {String} name - name of the user
+ * @property {String} github-username - User's Github Username
+ * @property {String} email - Email that Taiga invite will be sent to
+ */
+const Member = new mongoose.Schema({
     name: String,
     'github-username': String,
     email: String
 });
 
-const project = new mongoose.Schema({
+/**
+ * Project is meta data and collaborator information for a project.
+ * @typedef {Object} Project
+ * @property {String} name - name of the team
+ * @property {String} github-url - Short hand link to the Github repos
+ * @property {String} taiga-slug - Shorthand link to Taiga board
+ * @property {Array} members - {Array} of {Member} with user meta data
+ */
+const Project = new mongoose.Schema({
     name: String,
     'github-url': String,
     'taiga-slug': String,
-    members: [Members]
+    members: [Member]
 });
 
 const connection = mongoose.createConnection('mongodb://localhost/NicestDashboard');
 
-const integration = connection.model('integration', project);
+const integration = connection.model('integration', Project);
 
+/**
+ * Stores a list of new projects to Mongo for CA Dashboard to use.
+ * @function ConfigureCaDashboard
+ * @param {Array} metaData - {Array} of {Project} with Project info
+ * @returns {Null} returns after completion
+ */
 module.exports = function (metaData) {
     integration.collection.insert(metaData, () => {
         return;
