d13703550d23477c7c966ee8897a396dabd302b3
Christian Murphy <Christian.Murphy@asu.edu>
2016-04-10 11:04:30 -0700

    update octokat
    replace fetch all shim with builtin fetch all

--- modules/code-project/handler/choose-repository.js
+++ modules/code-project/handler/choose-repository.js
@@ -22,38 +22,13 @@ function chooseRepository (request, reply) {
             .get('github-password')
     });
 
-    fetchAll(Github.me.repos.fetch).then((repos) => {
-        reply.view('modules/code-project/view/choose-repository', {repos});
-    });
-}
-
-// TODO: remove this after next Octokat update that will include a native fetch all
-/**
- * Octokat FetchAll Shim
- * @private
- * @author philschatz
- * @param {Function} fn - getter function
- * @param {Object} args - arguements to pass to getter function
- * @returns {Object[]} all resulting values
- */
-function fetchAll (fn, args) {
-    // Accumulated results
-    let acc = [];
-    const promise = new Promise((resolve, reject) => {
-        fn(args).then((val) => {
-            acc = acc.concat(val);
-            if (val.nextPage) {
-                return fetchAll(val.nextPage).then((valTwo) => {
-                    acc = acc.concat(valTwo);
-                    resolve(acc);
-                }, reject);
-            }
-
-            return resolve(acc);
-        }, reject);
-    });
-
-    return promise;
+    Github
+        .me
+        .repos
+        .fetchAll()
+        .then((repos) => {
+            reply.view('modules/code-project/view/choose-repository', {repos});
+        });
 }
 
 module.exports = chooseRepository;
