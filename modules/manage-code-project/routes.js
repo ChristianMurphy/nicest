1da61fa1b4b902b418c5f2f08d2e0c5c09c69f58
Benjamin Paothatat <bpaothat@asu.edu>
2017-03-01 19:23:34 -0700

    Updated routes for managecode project

--- modules/manage-code-project/routes.js
+++ modules/manage-code-project/routes.js
@@ -2,9 +2,14 @@
 
 const handleList = require('./handler/list');
 const handleRedirect = require('./handler/redirect');
-const handlerRemoveUserAction = require('./handler/removeUser');
-const handlerAddUserAction = require('./handler/addUser');
-const handlerTransferUserAction = require('./handler/transferUser');
+const handlerRemoveUserAction = require('./handler/remove-user');
+const handlerAddUserAction = require('./handler/add-user');
+const handlerTransferUserAction = require('./handler/transfer-user');
+const handlerGitHubLoginView = require('./handler/github-view');
+const handlerTaigaLoginView = require('./handler/taiga-view');
+const handlerTaigaLoginAction = require('./handler/taiga-action');
+const handlerConfirmView = require('./handler/confirm-view');
+const handlerConfirmAction = require('./handler/confirm');
 
 module.exports = [
     {
@@ -32,6 +37,30 @@ module.exports = [
         handler: handlerTransferUserAction,
         method: 'GET',
         path: '/recipe/manage-code-project/transfer'
+    },
+    {
+        handler: handlerGitHubLoginView,
+        method: 'GET',
+        path: '/recipe/manage-code-project/github-login'
+    },
+    {
+        handler: handlerTaigaLoginView,
+        method: 'GET',
+        path: '/recipe/manage-code-project/taiga-login'
+    },
+    {
+        handler: handlerTaigaLoginAction,
+        method: 'POST',
+        path: '/recipe/manage-code-project/taiga-login'
+    },
+    {
+        handler: handlerConfirmView,
+        method: 'GET',
+        path: '/recipe/manage-code-project/confirm'
+    },
+    {
+        handler: handlerConfirmAction,
+        method: 'POST',
+        path: '/recipe/manage-code-project/confirm'
     }
-
 ];
