b4a2f435f1553f8371709a106f62ea0cc235b071
Nelson Tran <nelson@nelsontran.com>
2017-04-01 23:47:02 -0700

    Construct payload to send to CAssess
    The CAssess team has decided on the JSON payload structure
    that they want Nicest to use:
    
    {
      "admins": [
        {
          "course": "string",
          "email": "string",
          "full_name": "string",
          "password": "string"
        }
      ],
      "course": "string",
      "end_date": "2017-04-05",
      "github_owner": "string",
      "github_token": "string",
      "slack_token": "string",
      "taiga_token": "string",
      "teams": [
        {
          "channels": [
            {
              "course": "string",
              "id": "string",
              "team_name": "string"
            }
          ],
          "course": "string",
          "github_repo_id": "string",
          "slack_team_id": "string",
          "students": [
            {
              "course": "string",
              "email": "string",
              "full_name": "string",
              "password": "string",
              "team_name": "string"
            }
          ],
          "taiga_project_slug": "string",
          "team_name": "string"
        }
      ]
    }
    
    Course modules will now have to provide an 'end-date' attribute.

--- modules/code-project/task/configure-ca-dashboard.js
+++ modules/code-project/task/configure-ca-dashboard.js
@@ -4,62 +4,133 @@
  * @module code-project/task/configure-ca-dashboard
  */
 
-const mongoose = require('mongoose');
+const User = require('../../user/model/user');
+const Team = require('../../team/model/team');
+const Course = require('../../course/model/course');
 
 /**
- * Member is metadata for a user
- * @typedef {Object} Member
- * @property {String} name - name of the user
- * @property {String} github-username - User's Github Username
- * @property {String} email - Email that Taiga invite will be sent to
- */
-const Member = new mongoose.Schema({
-    email: String,
-    'github-username': String,
-    name: String
-});
-
-/**
- * Member is metadata for an instructor
- * @typedef {Object} Instructor
- * @property {String} name - name of the user
- * @property {String} email - Email address
- */
-const Instructor = new mongoose.Schema({
-    email: String,
-    name: String
-});
-
-/**
- * Project is meta data and collaborator information for a project.
- * @typedef {Object} Project
- * @property {String} name - name of the team
- * @property {String} github-url - Short hand link to the Github repos
- * @property {String} taiga-slug - Shorthand link to Taiga board
- * @property {Array<Object>} members - {Array} of {Member} with user meta data
- * @property {Array<String>} instructors - {Array} of {String} with instructor ids
- */
-const Project = new mongoose.Schema({
-    'github-url': String,
-    instructors: [Instructor],
-    members: [Member],
-    name: String,
-    'taiga-slug': String
-});
-
-const connection = mongoose.createConnection('mongodb://localhost/NicestDashboard');
-
-const integration = connection.model('integration', Project);
-
-/**
- * Stores a list of new projects to Mongo for CA Dashboard to use.
+ * Send project metadata to CAssess via an HTTP POST method.
+ * @param {String} cassessUsername - CAssess admin username
+ * @param {String} cassessPassword = CAssess admin password
+ * @param {String} cassessUrl - CAssess base URL
  * @param {Array} metaData - {Array} of {Project} with Project info
  * @returns {Null} returns after completion
  */
-function configureCaDashboard (metaData) {
-    integration
-        .collection
-        .insert(metaData, () => null);
+function configureCaDashboard (cassessUsername, cassessPassword, cassessUrl, metaData) {
+    const [project] = metaData;
+    const githubOwner = project['github-url'].substr(0, project['github-url'].indexOf('/'));
+
+    let courseName = null;
+    let endDate = null;
+
+    const teams = [];
+    const admins = [];
+
+    Course
+        .findOne({_id: project.course})
+        .exec()
+        .then((course) => {
+            courseName = course.name;
+            endDate = course.modules['end-date'];
+
+            const promises = [];
+
+            // Query metadata for each team.
+            for (let teamIndex = 0; teamIndex < metaData.length; teamIndex += 1) {
+                const teamMetadata = {};
+                const studentMetadata = [];
+
+                promises.push(
+                Team
+                    .findOne({_id: metaData[teamIndex].team})
+                    .exec()
+                    .then((team) => {
+                        teamMetadata['team-name'] = team.name;
+
+                        // Collect promises for all user queries.
+                        const userPromises = [];
+
+                        // Get list of student metadata for each member in team.
+                        for (let userIndex = 0; userIndex < metaData[teamIndex].members.length; userIndex += 1) {
+                            userPromises.push(
+                            User
+                                .findOne({_id: metaData[teamIndex].members[userIndex]})
+                                .exec()
+                                .then((student) => {
+                                    studentMetadata.push({
+                                        email: student.modules.cassess.email,
+                                        full_name: student.name,
+                                        password: student.modules.cassess.password
+                                    });
+                                })
+                            );
+                        }
+
+                        // Get instructor metadata.
+                        for (let userIndex = 0; userIndex < metaData[teamIndex].instructors.length; userIndex += 1) {
+                            userPromises.push(
+                            User
+                                .findOne({_id: metaData[teamIndex].instructors[userIndex]})
+                                .exec()
+                                .then((instructor) => {
+                                    // Instructors are the same for all teams.
+                                    if (teamIndex === 0) {
+                                        admins.push({
+                                            email: instructor.modules.cassess.email,
+                                            full_name: instructor.name,
+                                            password: instructor.modules.cassess.password
+                                        });
+                                    }
+                                })
+                            );
+                        }
+
+                        return Promise.all(userPromises).then(() => {
+                            teamMetadata.students = studentMetadata;
+                        });
+                    })
+                    .then(() => {
+                        const team = metaData[teamIndex];
+
+                        // Get only the repository name.
+                        const url = team['github-url'];
+                        const githubRepo = url.substr(url.indexOf('/') + 1, url.length);
+
+                        // Get slack channel IDs for a team.
+                        const channels = [];
+
+                        for (let userIndex = 0; userIndex < team['slack-groups'].length; userIndex += 1) {
+                            channels.push({id: team['slack-groups'][userIndex]});
+                        }
+
+                        teamMetadata.students = studentMetadata;
+                        teamMetadata.channels = channels;
+                        teamMetadata.taiga_project_slug = team['taiga-slug'];
+                        teamMetadata.github_repo = githubRepo;
+
+                        teams.push(teamMetadata);
+                    })
+                );
+            }
+
+            return Promise.all(promises);
+        })
+
+        // Send POST request to CAssess.
+        .then(() => {
+            const payload = {
+                admins,
+                course: courseName,
+                'end-date': endDate,
+                'github-owner': githubOwner,
+                'github-token': project['github-token'],
+                'slack-token': project['slack-token'],
+                'taiga-token': project['taiga-token'],
+                teams
+            };
+
+            console.log(payload);
+        });
 }
 
 module.exports = configureCaDashboard;
