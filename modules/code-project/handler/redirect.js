cbc6666877d493864086b0093eaf4b9b65c71166
Christian Murphy <Christian.Murphy.42@gmail.com>
2017-05-29 15:43:49 -0700

    Use GitHub OAuth as primary authentication throughout
    Replace all usages of Github password with GitHub OAuth token
    Remove GitHub login view, it is now replaced by OAuth page
    Upgrade npm dependencies
    Add a npm package lock file to ensure faster and easier to reproduce installs
    Use gitignore as eslint ignore file
    Remove remark validate links, it was causing missing dependency errors during test

--- modules/code-project/handler/redirect.js
+++ modules/code-project/handler/redirect.js
@@ -12,18 +12,8 @@
  */
 function redirect (request, reply) {
     const {prefix} = request.route.realm.modifiers.route;
-    const githubUsername = request
-        .yar
-        .get('github-username');
-    const githubPassword = request
-        .yar
-        .get('github-password');
 
-    if (typeof githubUsername === 'string' && typeof githubPassword === 'string') {
-        reply().redirect(`${prefix}/recipe/code-project/choose-course`);
-    } else {
-        reply().redirect(`${prefix}/recipe/github/login?next=${prefix}/recipe/code-project/choose-course`);
-    }
+    reply().redirect(`${prefix}/recipe/code-project/choose-course`);
 }
 
 module.exports = redirect;
--- modules/github/route.js
+++ modules/github/route.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const handleList = require('./handler/list');
-const handleLoginAction = require('./handler/login-action');
-const handleLoginView = require('./handler/login-view');
 const handleRedirect = require('./handler/redirect');
-const Joi = require('joi');
 
 module.exports = [
     {
@@ -12,43 +9,6 @@ module.exports = [
         handler: handleRedirect,
         method: 'GET',
         path: '/recipe/github'
-    },
-    {
-        config: {
-            validate: {
-                query: {
-                    failed: Joi
-                        .boolean()
-                        .default(false),
-                    next: Joi
-                        .string()
-                        .regex(/^\/[a-z0-9/-]+$/i, 'internal url')
-                        .empty(null)
-                }
-            }
-        },
-        handler: handleLoginView,
-        method: 'GET',
-        path: '/recipe/github/login'
-    },
-    {
-        config: {
-            validate: {
-                payload: {
-                    password: Joi.string(),
-                    redirect: Joi
-                        .string()
-                        .regex(/^\/[a-z0-9/-]+$/i, 'internal url')
-                        .empty(null),
-                    username: Joi
-                        .string()
-                        .alphanum()
-                }
-            }
-        },
-        handler: handleLoginAction,
-        method: 'POST',
-        path: '/recipe/github/login'
     },
     {
         handler: handleList,
